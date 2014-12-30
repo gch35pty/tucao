406146329a8a7cd1c4c907404ef84ed4e241762c
ChenKaiyan <fluxaymlovey@gmail.com>
2014-12-30 19:24:13 +0800

    a lot of icons

--- Web/TucaoApp/www/main.js
+++ Web/TucaoApp/www/main.js
@@ -345,6 +345,62 @@ function startEdit() {
 }
 
 
-function startTucaoPanel(){
-	
+function startTucao(){
+	$("#navbar").css("height","35px");
+}
+function closeTucao(){
+	$("#navbar").css("height","45px");
+}
+
+function startMylist(){
+	$.ajax({
+		type : "POST",
+		url : webRoot + 'nearnew',
+		data : {
+			"user_id" : userId,
+			"distance" : distance,
+			"offset" : 0,
+			"length" : -1,
+			"lat" : position.coords.latitude,
+			"lng" : position.coords.longitude
+		},
+		success : function(data) {
+			if (data.success) {
+				// alert(JSON.stringify(data));
+				var tpl = document.getElementById('tpl').innerHTML;
+				var html = juicer(tpl, data);
+				// document.getElementById("newMain").firstChild.innerHTML = html;
+				$("#mylistPanel .afScrollPanel").html(html);
+			} else {
+				alert("error");
+			}
+		},
+		dataType : "json"
+	});
+}
+function startMycomment(){
+	$.ajax({
+		type : "POST",
+		url : webRoot + 'nearnew',
+		data : {
+			"user_id" : userId,
+			"distance" : distance,
+			"offset" : 6,
+			"length" : 3,
+			"lat" : position.coords.latitude,
+			"lng" : position.coords.longitude
+		},
+		success : function(data) {
+			if (data.success) {
+				// alert(JSON.stringify(data));
+				var tpl = document.getElementById('tpl').innerHTML;
+				var html = juicer(tpl, data);
+				// document.getElementById("newMain").firstChild.innerHTML = html;
+				$("#mycommentPanel .afScrollPanel").html(html);
+			} else {
+				alert("error");
+			}
+		},
+		dataType : "json"
+	});
 }
